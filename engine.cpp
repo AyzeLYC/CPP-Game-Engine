6257d756d06121c42b0b15d744fcfa5c923e999e
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2023-03-17 18:32:53 +0100

    Updated engine.cpp

--- engine.cpp
+++ engine.cpp
@@ -1,5 +1,37 @@
+#include <ctime>
+#include <fstream>
+#include <iostream>
+#include <std>
+#include <string>
+
+#include "2d/main.cpp"
+#include "3d/main.cpp"
+#include "audio/main.cpp"
+#include "inputs/main.cpp"
+#include "internet/main.cpp"
+#include "physics/main.cpp"
+#include "ray-tracing/main.cpp"
+#include "threading/main.cpp"
+#include "user-interface/main.cpp"
+
 namespace engine {
     
+    unsigned int start() {
+        
+        
+        
+    };
     
+    unsigned int pause() {
+        
+        
+        
+    };
+    
+    unsigned int stop() {
+        
+        
+        
+    };
     
 }
