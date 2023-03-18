b3a822d16600a72471579b157d439297d577cb7f
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2023-03-18 12:14:34 +0100

    Updated main.cpp

--- physics/main.cpp
+++ physics/main.cpp
@@ -1,5 +1,18 @@
+#include <cmath>
+
 namespace physics {
     
+    unsigned long long lightSpeed = 299792458;
     
+    unsigned int calculateSoundSpeed(unsigned int specificHeatsRatio, unsigned int gasConstant, float temperature) {
+        
+        double absoluteTemperature,
+               result;
+        
+        absoluteTemperature = 273.15 + temperature;
+        result = sqrt(specificHeatsRatio * gasConstant * (absoluteTemperature));
+        return result;
+        
+    };
     
 }
