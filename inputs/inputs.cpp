d93ddab42fc89e4bcc01b4cf3a7d39afa2063723
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2023-03-21 19:34:10 +0100

    Updated and renamed main.cpp to inputs.cpp

---
+++ inputs/inputs.cpp
@@ -0,0 +1,136 @@
+namespace inputs {
+    
+    unsigned char key;
+    
+    if (key == "a") {
+        
+        
+        
+    };
+    if (key == "b") {
+        
+        
+        
+    };
+    if (key == "c") {
+        
+        
+        
+    };
+    if (key == "d") {
+        
+        
+        
+    };
+    if (key == "e") {
+        
+        
+        
+    };
+    if (key == "f") {
+        
+        
+        
+    };
+    if (key == "g") {
+        
+        
+        
+    };
+    if (key == "h") {
+        
+        
+        
+    };
+    if (key == "i") {
+        
+        
+        
+    };
+    if (key == "j") {
+        
+        
+        
+    };
+    if (key == "k") {
+        
+        
+        
+    };
+    if (key == "l") {
+        
+        
+        
+    };
+    if (key == "m") {
+        
+        
+        
+    };
+    if (key == "n") {
+        
+        
+        
+    };
+    if (key == "o") {
+        
+        
+        
+    };
+    if (key == "p") {
+        
+        
+        
+    };
+    if (key == "q") {
+        
+        
+        
+    };
+    if (key == "r") {
+        
+        
+        
+    };
+    if (key == "s") {
+        
+        
+        
+    };
+    if (key == "t") {
+        
+        
+        
+    };
+    if (key == "u") {
+        
+        
+        
+    };
+    if (key == "v") {
+        
+        
+        
+    };
+    if (key == "w") {
+        
+        
+        
+    };
+    if (key == "x") {
+        
+        
+        
+    };
+    if (key == "y") {
+        
+        
+        
+    };
+    if (key == "z") {
+        
+        
+        
+    };
+    
+}
