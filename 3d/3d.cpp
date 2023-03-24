350153e755e0cf9293322a84148b9b4c85b5bdf2
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2023-03-24 14:32:36 +0100

    Updated and renamed main.cpp to 3d.cpp

---
+++ 3d/3d.cpp
@@ -0,0 +1,35 @@
+namespace threedimension {
+    
+    unsigned int cubeAreaEquation(unsigned int aSize, unsigned int bSize, unsigned int cSize) {
+        
+        unsigned long long result;
+        
+        result = xSize * ySize * zSize;
+        return result;
+        
+    };
+    
+    unsigned int pyramidAreaEquation(unsigned int aSize, unsigned int bSize, unsigned int cSize, unsigned int dSize) {
+        
+        
+        
+    };
+    
+    unsigned int tubeAreaEquation(unsigned int height, unsigned int diameter) {
+        
+        
+        
+    };
+    
+    unsigned int sphereCircumferenceEquation(uint1_t inputType, unsigned long long ) {
+        
+        
+        
+    };
+    unsigned int sphereAreaEquation(uint2_t inputType, unsigned float input) {
+        
+        unsigned bool result;
+        
+    };
+    
+}
