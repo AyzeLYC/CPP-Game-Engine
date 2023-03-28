a7cbc38a792ebf9f6325745ff43a14bcf4c5b207
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2023-03-28 10:48:39 +0200

    Updated tools.cpp

--- tools/tools.cpp
+++ tools/tools.cpp
@@ -1,5 +1,16 @@
+#include <string>
+
 namespace tools {
     
-    
+    unsigned int brush() {
+        
+        
+        
+    };
+    unsigned int color() {
+        
+        
+        
+    };
     
 }
