db5c76d5421483ad1f9e5b32ae3da29a53f95294
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2023-03-28 10:36:38 +0200

    Updated inputs.cpp

--- inputs/inputs.cpp
+++ inputs/inputs.cpp
@@ -1,185 +1,205 @@
+#include <string>
 #include <windows.h>
 
 namespace inputs {
     
-    unsigned char key;
-    
-    if (key == "0") {
+    unsigned int ethernet() {
         
         
         
     };
-    if (key == "1") {
+    unsigned int keyboard() {
+        
+        unsigned char key;
+        
+        if (key == "0") {
+            
+            
+            
+        };
+        if (key == "1") {
+            
+            
+            
+        };
+        if (key == "2") {
+            
+            
+            
+        };
+        if (key == "3") {
+            
+            
+            
+        };
+        if (key == "4") {
+            
+            
+            
+        };
+        if (key == "5") {
+            
+            
+            
+        };
+        if (key == "6") {
+            
+            
+            
+        };
+        if (key == "7") {
+            
+            
+            
+        };
+        if (key == "8") {
+            
+            
+            
+        };
+        if (key == "9") {
+            
+            
+            
+        };
+        if (key == "a") {
+            
+            
+            
+        };
+        if (key == "b") {
+            
+            
+            
+        };
+        if (key == "c") {
+            
+            
+            
+        };
+        if (key == "d") {
+            
+            
+            
+        };
+        if (key == "e") {
+            
+            
+            
+        };
+        if (key == "f") {
+            
+            
+            
+        };
+        if (key == "g") {
+            
+            
+            
+        };
+        if (key == "h") {
+            
+            
+            
+        };
+        if (key == "i") {
+            
+            
+            
+        };
+        if (key == "j") {
+            
+            
+            
+        };
+        if (key == "k") {
+            
+            
+            
+        };
+        if (key == "l") {
+            
+            
+            
+        };
+        if (key == "m") {
+            
+            
+            
+        };
+        if (key == "n") {
+            
+            
+            
+        };
+        if (key == "o") {
+            
+            
+            
+        };
+        if (key == "p") {
+            
+            
+            
+        };
+        if (key == "q") {
+            
+            
+            
+        };
+        if (key == "r") {
+            
+            
+            
+        };
+        if (key == "s") {
+            
+            
+            
+        };
+        if (key == "t") {
+            
+            
+            
+        };
+        if (key == "u") {
+            
+            
+            
+        };
+        if (key == "v") {
+            
+            
+            
+        };
+        if (key == "w") {
+            
+            
+            
+        };
+        if (key == "x") {
+            
+            
+            
+        };
+        if (key == "y") {
+            
+            
+            
+        };
+        if (key == "z") {
+            
+            
+            
+        };
+        
+    };
+    unsigned int mouse() {
         
         
         
     };
-    if (key == "2") {
-        
-        
-        
-    };
-    if (key == "3") {
-        
-        
-        
-    };
-    if (key == "4") {
-        
-        
-        
-    };
-    if (key == "5") {
-        
-        
-        
-    };
-    if (key == "6") {
-        
-        
-        
-    };
-    if (key == "7") {
-        
-        
-        
-    };
-    if (key == "8") {
-        
-        
-        
-    };
-    if (key == "9") {
-        
-        
-        
-    };
-    if (key == "a") {
-        
-        
-        
-    };
-    if (key == "b") {
-        
-        
-        
-    };
-    if (key == "c") {
-        
-        
-        
-    };
-    if (key == "d") {
-        
-        
-        
-    };
-    if (key == "e") {
-        
-        
-        
-    };
-    if (key == "f") {
-        
-        
-        
-    };
-    if (key == "g") {
-        
-        
-        
-    };
-    if (key == "h") {
-        
-        
-        
-    };
-    if (key == "i") {
-        
-        
-        
-    };
-    if (key == "j") {
-        
-        
-        
-    };
-    if (key == "k") {
-        
-        
-        
-    };
-    if (key == "l") {
-        
-        
-        
-    };
-    if (key == "m") {
-        
-        
-        
-    };
-    if (key == "n") {
-        
-        
-        
-    };
-    if (key == "o") {
-        
-        
-        
-    };
-    if (key == "p") {
-        
-        
-        
-    };
-    if (key == "q") {
-        
-        
-        
-    };
-    if (key == "r") {
-        
-        
-        
-    };
-    if (key == "s") {
-        
-        
-        
-    };
-    if (key == "t") {
-        
-        
-        
-    };
-    if (key == "u") {
-        
-        
-        
-    };
-    if (key == "v") {
-        
-        
-        
-    };
-    if (key == "w") {
-        
-        
-        
-    };
-    if (key == "x") {
-        
-        
-        
-    };
-    if (key == "y") {
-        
-        
-        
-    };
-    if (key == "z") {
+    unsigned int usb() {
         
         
         
