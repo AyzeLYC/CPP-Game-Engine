0e897e2cc12cf18aac56f7187aca1c681480b034
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2023-03-28 19:00:02 +0200

    Updated graphics.cpp

--- graphics/graphics.cpp
+++ graphics/graphics.cpp
@@ -1,7 +1,32 @@
 #include <fstream>
 #include <string>
 
+#include "../2d/2d.cpp"
+#include "../3d/3d.cpp"
+#include "../amd/fsr/fsr.cpp"
+#include "../apple/opencl/opencl.cpp"
 #include "../camera/camera.cpp"
+#include "../khronos-group/vulkan/vulkan.cpp"
+#include "../microsoft/directx/directx.cpp"
+#include "../nvidia/cuda/cuda.cpp"
+#include "../nvidia/dlss/dlss.cpp"
+#include "../nvidia/dsr/dsr.cpp"
+#include "../nvidia/flex/flex.cpp"
+#include "../nvidia/flow/flow.cpp"
+#include "../nvidia/iray/iray.cpp"
+#include "../nvidia/mdl/mdl.cpp"
+#include "../nvidia/mvr/mvr.cpp"
+#include "../nvidia/optix/optix.cpp"
+#include "../nvidia/physx/physx.cpp"
+#include "../nvidia/rtd/rtd.cpp"
+#include "../nvidia/rtxdi/rtxdi.cpp"
+#include "../nvidia/rtxgi/rtxgi.cpp"
+#include "../nvidia/tss/tss.cpp"
+#include "../nvidia/vrs/vrs.cpp"
+#include "../physics/physics.cpp"
+#include "../ray-tracing/ray-tracing.cpp"
+#include "../silicon-graphics/opengl/opengl.cpp"
+#include "../threading/threading.cpp"
 
 namespace graphics {
     
