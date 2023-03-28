08a455e4820bcc40ed32450bcf2e43d21db01a19
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2023-03-28 10:29:39 +0200

    Created graphics.cpp

---
+++ graphics/graphics.cpp
@@ -0,0 +1,37 @@
+#include <fstream>
+#include <string>
+
+#include "../camera/camera.cpp"
+
+namespace graphics {
+    
+    struct object {
+        
+        string name,
+               threedModelFileExtension,
+               threedModelsPositions[],
+               threedModels[],
+               textureFileExtension,
+               texturesPosition[],
+               textures[];
+        uint8_t opacity;
+        
+    }
+    
+    unsigned int start() {
+        
+        
+        
+    };
+    unsigned int pause() {
+        
+        
+        
+    };
+    unsigned int stop() {
+        
+        
+        
+    };
+    
+}
