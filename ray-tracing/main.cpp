c2ecce4f533d678e147075b00a3e3e53ebb6dd00
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2023-03-31 15:04:24 +0200

    Updated main.cpp

--- ray-tracing/main.cpp
+++ ray-tracing/main.cpp
@@ -1,6 +1,7 @@
 #include "../apple/opencl/opencl.cpp"
-#include "../nvidia/rtx/rtxdi.cpp"
-#include "../nvidia/rtx/rtxgi.cpp"
+#include "../khronos-group/vulkan/vulkan.cpp"
+#include "../nvidia/rtxdi/rtxdi.cpp"
+#include "../nvidia/rtxgi/rtxgi.cpp"
 #include "../silicon-graphics/opengl/opengl.cpp"
 
 namespace raytracing {
