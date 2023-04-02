6944411266e51da10071d08201212487d95f0cdf
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2023-04-02 10:27:11 +0200

    Updated engine.cpp

--- engine.cpp
+++ engine.cpp
@@ -3,37 +3,55 @@
 #include <iostream>
 #include <std>
 #include <string>
+#include <thread>
 
-#include "2d/main.cpp"
-#include "3d/main.cpp"
-#include "audio/main.cpp"
-#include "inputs/main.cpp"
-#include "internet/main.cpp"
-#include "physics/main.cpp"
-#include "ray-tracing/main.cpp"
-#include "threading/main.cpp"
-#include "user-interface/main.cpp"
+#include "2d/2d.cpp"
+#include "3d/3d.cpp"
+#include "audio/audio.cpp"
+#include "camera/camera.cpp"
+#include "decryption/decryption.cpp"
+#include "derivation/derivation.cpp"
+#include "encryption/encryption.cpp"
+#include "graphics/graphics.cpp"
+#include "hashing/hashing.cpp"
+#include "inputs/inputs.cpp"
+#include "internet/internet.cpp"
+#include "physics/physics.cpp"
+#include "player/player.cpp"
+#include "project/project.cpp"
+#include "user-interface/user-interface.cpp"
 
 namespace engine {
     
     unsigned int start(string args[]) {
         
-        
+        inputs.start();
+        internet.start();
+        twod.start();
+        threed.start();
+        player.start();
+        physics.start();
+        camera.start();
+        raytracing.start();
+        decryption.start();
+        derivation.start();
+        encryption.start();
+        hashing.start();
+        internet.start();
+        userinterface.start();
+        audio.start();
         
     };
-    
     unsigned int pause(string args[]) {
         
         
         
     };
-    
     unsigned int unpause(string args[]) {
         
         
         
     };
-    
     unsigned int stop(string args[]) {
         
         
