5122f7258f3f2a76ca648afeabea3bc6648d996c
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2023-04-02 10:21:18 +0200

    Updated user-interface.cpp

--- user-interface/user-interface.cpp
+++ user-interface/user-interface.cpp
@@ -4,6 +4,51 @@
 
 namespace ui {
     
+    unsigned int start() {
+        
+        
+        
+    };
+    unsigned int pause() {
+        
+        
+        
+    };
+    unsigned int unpause() {
+        
+        
+        
+    };
+    unsigned int stop() {
+        
+        
+        
+    };
     
+    unsigned itn consoleInterface() {
+        
+        string xSize,
+               ySize,
+               buttons[],
+               borders[],
+               backgroundColor;
+        
+    };
+    unsigned int gameInterface() {
+        
+        string xSize,
+               ySize,
+               borders[];
+        
+    };
+    unsigned int toolbarInterface() {
+        
+        string xSize,
+               ySize,
+               buttons[],
+               borders[],
+               backgroundColor;
+        
+    };
     
 }
