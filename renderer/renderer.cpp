a3bdefcac17d08ee1f4ae675c3f11001df2444ce
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2023-04-05 16:17:40 +0200

    Created renderer.cpp

---
+++ renderer/renderer.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <thread>
+
+#include "../2d/2d.cpp"
+#include "../3d/3d.cpp"
+#include "../graphics/graphics.cpp"
+#include "../physics/physics.cpp"
+
+namespace renderer {
+    
+    
+    
+}
