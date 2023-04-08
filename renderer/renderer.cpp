3762615ce95bbc71f1d6533af96b6a9061a79565
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2023-04-08 10:51:26 +0200

    Updated renderer.cpp

--- renderer/renderer.cpp
+++ renderer/renderer.cpp
@@ -3,8 +3,10 @@
 
 #include "../2d/2d.cpp"
 #include "../3d/3d.cpp"
+#include "../camera/camera.cpp"
 #include "../graphics/graphics.cpp"
 #include "../physics/physics.cpp"
+#include "../player/player.cpp"
 
 namespace renderer {
     
