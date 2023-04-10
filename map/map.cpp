a150e15643a96d38bff3c4588d06631ca2f504a1
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2023-04-10 11:31:14 +0200

    Created map.cpp

---
+++ map/map.cpp
@@ -0,0 +1,20 @@
+#include <fstream>
+#include <string>
+
+namespace map {
+    
+    unsigned int generate(uint1_t random, string name, string seed) {
+        
+        if (random == 0) {
+            
+            string fileContent = ""
+            
+        } else {
+            
+            
+            
+        };
+        
+    };
+    
+}
