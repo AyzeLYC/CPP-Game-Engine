0337dec54c79099565fda75fea0bd5c5ca141ff6
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2023-04-12 11:39:46 +0200

    Created parameters.cpp

---
+++ parameters/parameters.cpp
@@ -0,0 +1,24 @@
+#include <fstream>
+#include <std>
+#include <string>
+
+namespace parameters {
+    
+    struct parametersFileDatas {
+        
+        uint8_t fileVersion;
+        uint16_t fps;
+        string parametersName;
+        
+    }
+    
+    unsigned int createNewParametersFile(parametersFileDatas DATAS) {
+        
+        ofstream parametersFile("parameters.json");
+      
+        parametersFile << DATAS << std::endl;
+        parametersFile.close();
+        
+    };
+    
+}
