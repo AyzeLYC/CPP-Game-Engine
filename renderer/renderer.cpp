ed50d68089c6e411c20be3b8b3601d670df358b8
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2023-04-12 11:16:13 +0200

    Updated renderer.cpp

--- renderer/renderer.cpp
+++ renderer/renderer.cpp
@@ -5,11 +5,16 @@
 #include "../3d/3d.cpp"
 #include "../camera/camera.cpp"
 #include "../graphics/graphics.cpp"
+#include "../parameters/parameters.cpp"
 #include "../physics/physics.cpp"
 #include "../player/player.cpp"
 
 namespace renderer {
     
-    
+    for (bool i = true; i = true; i = true) {
+        
+        sleep(1000 / parameters.FPS);
+        
+    };
     
 }
