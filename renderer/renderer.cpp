ca5e8fa1a0c67363a44b7aa738d936ca2d8ac892
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2023-04-19 09:50:50 +0200

    Updated renderer.cpp

--- renderer/renderer.cpp
+++ renderer/renderer.cpp
@@ -5,14 +5,16 @@
 #include "../3d/3d.cpp"
 #include "../camera/camera.cpp"
 #include "../graphics/graphics.cpp"
+#include "../main.cpp"
 #include "../parameters/parameters.cpp"
 #include "../physics/physics.cpp"
 #include "../player/player.cpp"
 
 namespace renderer {
     
-    for (bool i = true; i = true; i = true) {
+    for (uint1_t i; main.; i) {
         
+        graphics.compute(physics.calculateCollisions(object.objects) + map.map + player.players);
         sleep(1000 / parameters.FPS);
         
     };
