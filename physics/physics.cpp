c8db0f7cebb731bc28d29339f2fca0a5b78ba53b
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2023-04-26 20:29:07 +0200

    Updated physics.cpp

--- physics/physics.cpp
+++ physics/physics.cpp
@@ -16,9 +16,18 @@ namespace physics {
         return result;
         
     };
-    unsigned int calculateCollisions(unsigned int fps, unsigned long long xPositions[], unsigned long long yPositions[], unsigned long long zPositions[], unsigned long long speeds[], unsigned int objectsMass[], unsigned int directionsDegrees[]) {
+    unsigned int calculateCollision(unsigned int fps, unsigned int screenXSize, unsigned int screenYSize unsigned long long xPositions[2], unsigned long long yPositions[2], unsigned long long ePositions[2], unsigned long long speeds[2], unsigned long long objectsMass[2], unsigned int directionsDegrees[2]) {
         
-        for () {
+        for (unsigned long long i; i < (2^(8*8)) || xPositions[0 : 1] < screenXSize + 1 || yPositions[0 : 1] < screenYSize + 1 || speeds[0 : 1] != 0; i++) {
+            
+            sleep(1000 / fps);
+            
+        };
+        
+    };
+    unsigned int calculateCollisions(unsigned int fps, unsigned int screenXSize, unsigned int screenYSize, unsigned long long xPositions[], unsigned long long yPositions[], unsigned long long zPositions[], unsigned long long speeds[], unsigned long long objectsMass[], unsigned int directionsDegrees[]) {
+        
+        for (unsigned long long i; i < (2^(8*8)) || xPositions[0 : xPositions.length() - 1] < screenXSize + 1 || yPositions[0 : yPositions.length() - 1] < screenYSize + 1 || speeds[0 : speeds.length() - 1] != 0; i++) {
             
             sleep(1000 / fps);
             
