3ac93f82390eacd53ef35383faaff917d07fb887
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2023-04-27 23:53:22 +0200

    Updated ide.cpp

--- ide/ide.cpp
+++ ide/ide.cpp
@@ -1,5 +1,17 @@
+#include <array>
+#include <string>
+#include <thread>
+
 namespace ide {
     
-    
+    unsigned int compile(string codeLines) {
+        
+        for (i = new uint8_t<round(code.length()) + 1>; i < code.length(); i++) {
+            
+            
+            
+        };
+        
+    };
     
 }
