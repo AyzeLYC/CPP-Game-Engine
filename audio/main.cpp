275bc7e07e088b43e3cfba138aebb7d76e4ad273
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2023-04-30 11:33:25 +0200

    Updated main.cpp

--- audio/main.cpp
+++ audio/main.cpp
@@ -1,5 +1,46 @@
+#include <fstream>
+#include <string>
+#include <vector>
+
+#include "../files/files.cpp"
+#include "../object/object.cpp"
+
 namespace audio {
     
+    namespace audioLocation {
+        
+        object.vector3 position;
+        object.vector3 rotation;
+        
+    };
     
+    unsigned int createAudioFile(string filePath, string fileName, strilg fileContent) {
+        
+        
+        
+    };
+    unsigned int openAudioFile(string filePath, string fileName, string fileDataFormat) {
+        
+        string fileContent;
+        
+        fileContent = files.openFile(filePath, str(fileName + "." + fileDataFormat));
+        
+    };
+    unsigned int readAudioFile(string fileContent) {
+        
+        
+        
+    };
+    unsigned int deleteAudioFile(string filePath, string fileName) {
+        
+        
+        
+    };
+    
+    unsigned int spacializeAudio(string sounds[], audioLocation soundsLocations[]) {
+        
+        
+        
+    };
     
 }
