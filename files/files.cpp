20f51f412d50624e1320dbf9a1c10025c2c62adc
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2023-04-30 11:27:30 +0200

    Created files.cpp

---
+++ files/files.cpp
@@ -0,0 +1,25 @@
+#include <fstream>
+#include <string>
+#include <winuser.h>
+
+namespace files {
+    
+    string defaultFilesPath = "C:\Users\" + +"\AppData\Roaming";
+    
+    unsigned int createFile(string filePath, string fileName, string fileContent) {
+        
+        
+        
+    };
+    unsigned int openFile(string filePath, string fileName) {
+        
+        
+        
+    };
+    unsigned int deleteFile(string filePath, string fileName) {
+        
+        
+        
+    };
+    
+}
