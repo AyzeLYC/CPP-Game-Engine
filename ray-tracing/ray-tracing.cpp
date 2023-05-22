e4b577f25c74895fa1efff68586c975674ae91fb
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2023-05-22 17:16:52 +0200

    Updated and moved /ray-tracing/main.cpp to /ray-tracing/ray-tracing.cpp

--- ray-tracing/ray-tracing.cpp
+++ ray-tracing/ray-tracing.cpp
@@ -2,10 +2,15 @@
 #include "../khronos-group/vulkan/vulkan.cpp"
 #include "../nvidia/rtxdi/rtxdi.cpp"
 #include "../nvidia/rtxgi/rtxgi.cpp"
+#include "../object/object.cpp"
 #include "../silicon-graphics/opengl/opengl.cpp"
 
 namespace raytracing {
     
-    
+    unsigned int raycast(vector3object OBJECTS[], vector3 raysPosition[]) {
+        
+        
+        
+    };
     
 }
